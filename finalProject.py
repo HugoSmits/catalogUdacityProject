45475cc003d6f5e686723de92ac24ba075594de1
Hugo Sebastiaan Smits <hugo.smits1995@gmail.com>
2019-01-03 15:55:03 +0000

    test region

--- finalProject.py
+++ finalProject.py
@@ -20,6 +20,11 @@ import requests
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 
+#from flask.ext.httpauth import HTTPBasicAuth
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
+
 app = Flask(__name__)
 
 engine = create_engine('sqlite:///restaurantmenuwithusers.db',
@@ -35,6 +40,135 @@ APPLICATION_NAME = "Restaurant Menu Application"
 
 # Create anti-forgery state token
 
+@app.route('/oauth/<provider>', methods = ['POST'])
+def login(provider):
+    #STEP 1 - Parse the auth code
+    auth_code = request.json.get('auth_code')
+    print "Step 1 - Complete, received auth code %s" % auth_code
+    if provider == 'google':
+        #STEP 2 - Exchange for a token
+        try:
+            # Upgrade the authorization code into a credentials object
+            oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+            oauth_flow.redirect_uri = 'postmessage'
+            credentials = oauth_flow.step2_exchange(auth_code)
+        except FlowExchangeError:
+            response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+            response.headers['Content-Type'] = 'application/json'
+            return response
+          
+        # Check that the access token is valid.
+        access_token = credentials.access_token
+        url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+        h = httplib2.Http()
+        result = json.loads(h.request(url, 'GET')[1])
+        # If there was an error in the access token info, abort.
+        if result.get('error') is not None:
+            response = make_response(json.dumps(result.get('error')), 500)
+            response.headers['Content-Type'] = 'application/json'
+            
+        # # Verify that the access token is used for the intended user.
+        # gplus_id = credentials.id_token['sub']
+        # if result['user_id'] != gplus_id:
+        #     response = make_response(json.dumps("Token's user ID doesn't match given user ID."), 401)
+        #     response.headers['Content-Type'] = 'application/json'
+        #     return response
+
+        # # Verify that the access token is valid for this app.
+        # if result['issued_to'] != CLIENT_ID:
+        #     response = make_response(json.dumps("Token's client ID does not match app's."), 401)
+        #     response.headers['Content-Type'] = 'application/json'
+        #     return response
+
+        # stored_credentials = login_session.get('credentials')
+        # stored_gplus_id = login_session.get('gplus_id')
+        # if stored_credentials is not None and gplus_id == stored_gplus_id:
+        #     response = make_response(json.dumps('Current user is already connected.'), 200)
+        #     response.headers['Content-Type'] = 'application/json'
+        #     return response
+        print "Step 2 Complete! Access Token : %s " % credentials.access_token
+
+        #STEP 3 - Find User or make a new one
+        
+        #Get user info
+        h = httplib2.Http()
+        userinfo_url =  "https://www.googleapis.com/oauth2/v1/userinfo"
+        params = {'access_token': credentials.access_token, 'alt':'json'}
+        answer = requests.get(userinfo_url, params=params)
+      
+        data = answer.json()
+
+        name = data['name']
+        picture = data['picture']
+        email = data['email']
+        
+        
+     
+        #see if user exists, if it doesn't make a new one
+        user = session.query(User).filter_by(email=email).first()
+        if not user:
+            user = User(username = name, picture = picture, email = email)
+            session.add(user)
+            session.commit()
+
+        
+
+        #STEP 4 - Make token
+        token = user.generate_auth_token(600)
+
+        
+
+        #STEP 5 - Send back token to the client 
+        return jsonify({'token': token.decode('ascii')})
+        
+        #return jsonify({'token': token.decode('ascii'), 'duration': 600})
+    else:
+        return 'Unrecoginized Provider'
+        
+@auth.verify_password
+def verify_password(username_or_token, password):
+    #Try to see if it's a token first
+    user_id = User.verify_auth_token(username_or_token)
+    if user_id:
+        user = session.query(User).filter_by(id = user_id).one()
+    else:
+        user = session.query(User).filter_by(username = username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
+
+@app.route('/api/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if session.query(User).filter_by(username = username).first() is not None:
+        abort(400) # existing user
+    user = User(username = username)
+    user.hash_password(password)
+    session.add(user)
+    session.commit()
+    return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = session.query(User).filter_by(id=id).one()
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({ 'data': 'Hello, %s!' % g.user.username })
 
 @app.route('/login')
 def showLogin():
@@ -297,6 +431,52 @@ def getUserID(email):
     except BaseException:
         return None
 
+@app.route('/restaurants/JSON', methods = ['GET', 'POST'])
+def all_restaurants_handler():
+  if request.method == 'GET':
+    # RETURN ALL RESTAURANTS IN DATABASE
+    restaurants = session.query(Restaurant).all()
+    return jsonify(restaurants = [i.serialize for i in restaurants])
+
+  elif request.method == 'POST':
+    # MAKE A NEW RESTAURANT AND STORE IT IN DATABASE
+    location = request.args.get('location', '')
+    mealType = request.args.get('mealType', '')
+    restaurant_info = findARestaurant(mealType, location)
+    if restaurant_info != "No Restaurants Found":
+      restaurant = Restaurant(restaurant_name = unicode(restaurant_info['name']), restaurant_address = unicode(restaurant_info['address']), restaurant_image = restaurant_info['image'])
+      session.add(restaurant)
+      session.commit() 
+      return jsonify(restaurant = restaurant.serialize)
+    else:
+      return jsonify({"error":"No Restaurants Found for %s in %s" % (mealType, location)})
+    
+@app.route('/restaurants/JSON/<int:id>', methods = ['GET','PUT', 'DELETE'])
+def restaurant_handler(id):
+  restaurant = session.query(Restaurant).filter_by(id = id).one()
+  if request.method == 'GET':
+    #RETURN A SPECIFIC RESTAURANT
+    return jsonify(restaurant = restaurant.serialize)
+  elif request.method == 'PUT':
+    #UPDATE A SPECIFIC RESTAURANT
+    address = request.args.get('address')
+    image = request.args.get('image')
+    name = request.args.get('name')
+    if address:
+        restaurant.restaurant_address = address
+    if image:
+        restaurant.restaurant_image = image
+    if name:
+        restaurant.restaurant_name = name
+    session.commit()
+    return jsonify(restaurant = restaurant.serialize)
+
+  elif request.method == 'DELETE':
+    #DELETE A SPECFIC RESTAURANT
+    session.delete(restaurant)
+    session.commit()
+    return "Restaurant Deleted"
+
 @app.route('/restaurants/JSON')
 def restaurantJSON():
     restaurant = session.query(Restaurant).all()
@@ -531,6 +711,7 @@ def deleteMenuItem(restaurant_id, menu_id):
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
+    #app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     app.config['SESSION_TYPE'] = 'filesystem'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
